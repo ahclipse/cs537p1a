32d884c469e3ef168ea50ddc9ba13ea407fcf16a
Adam <ahclipse@gmail.com>
2015-02-08 13:30:05 -0600

    Initialize

---
+++ multTable.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char * argv[])
+{
+	if (argc <= 1)
+	{
+		printf("Usage: p1a2 string1 [stringN]\n");
+		exit(1);
+	}
+
+	// YOUR CODE HERE
+
+	return 0;
+}
+
